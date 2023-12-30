9fdfeaa450de92217b4c63dd770d1f732b2619fa
Rodrigo Rael <rodrigorael53@gmail.com>
2023-12-30 13:28:33 -0300

    :sparkles: feat: users routes

--- node-2024/aulas/01-fundamentos-nodejs/src/database.js
+++ node-2024/aulas/01-fundamentos-nodejs/src/database.js
@@ -17,8 +17,16 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
-    const data = this.#database[table] ?? []
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
 
     return data
   }
@@ -33,5 +41,23 @@ export class Database {
     this.#persist()
 
     return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data }
+      this.#persist()
+    }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
   }
 }
--- node-2024/aulas/01-fundamentos-nodejs/src/server.js
+++ node-2024/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,34 +1,24 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
-
-const database = new Database()
+import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find(route => {
+    return route.method === method && route.path.test(url)
+  })
 
-    return res
-      .end(JSON.stringify(users))
-  }
+  if (route) {
+    const routeParams = req.url.match(route.path)
+    const { query, ...params } = routeParams.groups
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
