5a19e501cd3f24bf32522a755c55b5bd053de779
Rodrigo Rael <rodrigorael53@gmail.com>
2024-01-07 08:25:40 -0300

    :sparkles: feat: question and answer CRUD

--- node-2024/aulas/04-clean-ddd/src/domain/forum/application/repositories/questions-repository.ts
+++ node-2024/aulas/04-clean-ddd/src/domain/forum/application/repositories/questions-repository.ts
@@ -1,6 +1,9 @@
 import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export interface QuestionsRepository {
-  create(question: Question): Promise<void>
+  findById(id: string): Promise<Question | null>
   findBySlug(slug: string): Promise<Question | null>
+  save(question: Question): Promise<void>
+  create(question: Question): Promise<void>
+  delete(question: Question): Promise<void>
 }
