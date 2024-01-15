0f52e0d18cb99aee191342a5a8cda88d57246eab
Rodrigo Rael <rodrigorael53@gmail.com>
2024-01-15 08:28:39 -0300

    :test_tube: test: E2E tests with vitest

---
+++ node-2024/aulas/05-nest-clean/vitest.config.e2e.ts
@@ -0,0 +1,19 @@
+import swc from 'unplugin-swc'
+import { configDefaults, defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+    exclude: [...configDefaults.exclude, '**/data/pg/**'],
+    setupFiles: ['./test/setup-e2e.ts'],
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+})
