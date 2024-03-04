83dd1f2b597b1d3a15ac56aea472c5f4342ccf09
Rodrigo Rael <rodrigorael53@gmail.com>
2024-03-04 04:49:59 -0300

    :sparkles: feat: notification domain

---
+++ node-2024/desafios/ignite-challenge-04/src/domain/notification/application/subscribers/on-order-posted.ts
@@ -0,0 +1,28 @@
+import { DomainEvents } from '@/core/events/domain-events';
+import { EventHandler } from '@/core/events/event-handler';
+import { OrderPostedEvent } from '@/domain/order/enterprise/events/order-posted';
+import { Injectable } from '@nestjs/common';
+
+import { SendNotificationUseCase } from '../use-cases/send-notification';
+
+@Injectable()
+export class OnOrderPosted implements EventHandler {
+  constructor(private sendNotification: SendNotificationUseCase) {
+    this.setupSubscriptions();
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      this.sendOrderPostedNotification.bind(this),
+      OrderPostedEvent.name,
+    );
+  }
+
+  private async sendOrderPostedNotification({ order }: OrderPostedEvent) {
+    await this.sendNotification.execute({
+      recipientId: order.customerId.toString(),
+      title: `Novo status em "${order.description}"`,
+      content: `A encomenda "${order.description}" foi postada.`,
+    });
+  }
+}
---
+++ node-2024/desafios/ignite-challenge-04/src/domain/notification/application/subscribers/on-order-withdrawn.ts
@@ -0,0 +1,28 @@
+import { DomainEvents } from '@/core/events/domain-events';
+import { EventHandler } from '@/core/events/event-handler';
+import { OrderWithdrawnEvent } from '@/domain/order/enterprise/events/order-withdrawn';
+import { Injectable } from '@nestjs/common';
+
+import { SendNotificationUseCase } from '../use-cases/send-notification';
+
+@Injectable()
+export class OnOrderWithdrawn implements EventHandler {
+  constructor(private sendNotification: SendNotificationUseCase) {
+    this.setupSubscriptions();
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      this.sendOrderWithdrawnNotification.bind(this),
+      OrderWithdrawnEvent.name,
+    );
+  }
+
+  private async sendOrderWithdrawnNotification({ order }: OrderWithdrawnEvent) {
+    await this.sendNotification.execute({
+      recipientId: order.customerId.toString(),
+      title: `Novo status em "${order.description}"`,
+      content: `A encomenda "${order.description}" foi retirada para entrega.`,
+    });
+  }
+}
